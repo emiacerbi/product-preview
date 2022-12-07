1f59d1be594b5952d48cf3a3bfb2285938d21f6e
Emiliano Acerbi <eacerbip@gmail.com>
2022-12-07 10:44:44 -0300

    wrapper component

---
+++ src/components/Wrapper.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react'
+
+type Props = {
+  children: ReactNode
+}
+
+const Wrapper = ({ children }: Props) => {
+  return (
+    <div className="p-4 py-6 bg-neutral-100 grid place-content-center min-h-screen font-primary">
+      {children}
+    </div>
+  )
+}
+
+export default Wrapper
